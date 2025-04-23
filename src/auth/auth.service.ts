0f011a03510d116a032b4b09e6f5428fa3819614
hungdinh-dev <baohungdinhnguyen1412@gmail.com>
2025-04-23 18:30:31 +0700

    Step 6

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '@/modules/users/users.service';
+import { comparePasswordHelper } from '@/helpers/util';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
+
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(username);
+
+    console.log("View user? ", user)
+
+    const isValidPassword = await comparePasswordHelper(password, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException("Username/Password không hợp lệ.");
+    }
+
+    const payload = { sub: user._id, username: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+
+  }
+}
