0f431427dd7a9500745b271d361dacded8a38c6b
hungdinh-dev <baohungdinhnguyen1412@gmail.com>
2025-04-23 22:31:02 +0700

    Step 7

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,21 +10,19 @@ export class AuthService {
     private jwtService: JwtService
   ) { }
 
-  async signIn(username: string, password: string): Promise<any> {
+
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
-
-    console.log("View user? ", user)
-
-    const isValidPassword = await comparePasswordHelper(password, user.password);
-
-    if (!isValidPassword) {
-      throw new UnauthorizedException("Username/Password không hợp lệ.");
-    }
-
-    const payload = { sub: user._id, username: user.email };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
-
+    const isValidPassword = await comparePasswordHelper(pass, user.password);
+    if (!user || !isValidPassword) return null;
+    return user;
   }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
 }
